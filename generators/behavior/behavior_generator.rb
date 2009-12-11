be7468d3e4b525ffcc1b7e08d17a5c560828d0fc
Paul Campbell <paul@Paul-Campbells-MacBook-Pro.local>
2009-12-11 11:49:48 +0000

    create db/migrate if it doesn't exist

--- generators/behavior/behavior_generator.rb
+++ generators/behavior/behavior_generator.rb
@@ -1,6 +1,7 @@
 class BehaviorGenerator < Rails::Generator::Base
   def manifest
     record do |m|
+      m.directory 'db/migrate'
       m.file "20091210164854_create_behavior_configs.rb", "db/migrate/20091210164854_create_behavior_configs.rb"
       m.file "behavior.yml", "config/behavior.yml"
     end
